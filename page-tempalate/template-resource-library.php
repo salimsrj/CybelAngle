3133b170402034a9784d4b76e01e00092b613d1f
Mahmudul Hasan <salimsrj@gmail.com>
2023-02-23 07:44:08 +0600

    23 feb 23

---
+++ page-tempalate/template-resource-library.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * Template Name: Resource Library
+ *
+ * This template can be used to override the default template and sidebar setup
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+get_header();
+?>
+<section class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="left-content-box">
+                    <span class="pre-title">Resources</span>
+                    <h2 class="title">Resource library</h2>
+                    <p>Tool and strategies modern teams need to help their companies grow.</p>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="right-content-box">
+                    <div class="content">
+                        <p>CybelAngel has plenty of resources to get you up to speed on our technology, use cases, and
+                            platform. We also have a wide variety of original research covering cyber risks and
+                            preventative strategies.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    </div>
+</section>
+
+<section class="resource">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="resource-library">
+                    <div class="left-sidebar">
+                        <div class="widget">
+                            <div class="search-form">
+                                <form action="#">
+                                    <div class="input-group">
+                                        <input class="form-control border-end-0 border" type="search" value="search"
+                                            id="example-search-input">
+                                        <span class="input-group-append">
+                                            <button
+                                                class="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border ms-n5"
+                                                type="button">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="widget">
+                            <div class="left-menu">
+                                <ul>
+                                    <li>Resource Topics
+                                        <ul>
+                                            <li><a href="#">View all</a></li>
+                                            <li><a href="#">Shadow IT</a></li>
+                                            <li><a href="#">Third Party Risk</a></li>
+                                            <li><a href="#">Exposed Data</a></li>
+                                            <li><a href="#">Dark Web</a></li>
+                                            <li><a href="#">Culture</a></li>
+                                            <li><a href="#">Women in Cybersecurity</a></li>
+                                            <li><a href="#">Ransomware</a></li>
+                                            <li><a href="#">Data Leaks</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="right-content-box">
+                        <div class="resources">
+
+                            <div class="item">
+                                <article>
+                                    <div class="image-container" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/resource-img.jpg); background-size: cover;
+        background-position: center;">
+                                        <div class="post-meta">
+                                            <div class="left-box">
+                                                <span>1 min Read</span>
+                                                <span class="date">Jan 01, 2023</span>
+                                            </div>
+
+                                            <div class="right-box">
+                                                <span>Shadow IT</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h3 class="title">Healthcare Data Actively Targeted and Sold on the Dark Web</h3>
+                                    <p>How do you create compelling presentations that wow your colleagues and impress
+                                        your
+                                        managers?</p>
+                                    <a href="#" class="link">Read post</a>
+                                </article>
+                            </div>
+
+                            <div class="item">
+                                <article>
+                                    <div class="image-container" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/resource-img.jpg); background-size: cover;
+        background-position: center;">
+                                        <div class="post-meta">
+                                            <div class="left-box">
+                                                <span>1 min Read</span>
+                                                <span class="date">Jan 01, 2023</span>
+                                            </div>
+
+                                            <div class="right-box">
+                                                <span>Shadow IT</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h3 class="title">Healthcare Data Actively Targeted and Sold on the Dark Web</h3>
+                                    <p>How do you create compelling presentations that wow your colleagues and impress
+                                        your
+                                        managers?</p>
+                                    <a href="#" class="link">Read post</a>
+                                </article>
+                            </div>
+
+
+                            <div class="item">
+                                <article>
+                                    <div class="image-container" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/resource-img.jpg); background-size: cover;
+                                background-position: center;">
+                                        <div class="post-meta">
+
+                                            <div class="left-box">
+                                                <span>1 min Read</span>
+                                                <span class="date">Jan 01, 2023</span>
+                                            </div>
+
+                                            <div class="right-box">
+                                                <span>Shadow IT</span>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <h3 class="title">Healthcare Data Actively Targeted and Sold on the Dark Web</h3>
+                                    <p>How do you create compelling presentations that wow your colleagues and impress
+                                        your
+                                        managers?</p>
+                                    <a href="#" class="link">Read post</a>
+                                </article>
+                            </div>
+
+
+                            <div class="item">
+                                <article>
+                                    <div class="image-container" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/resource-img.jpg); background-size: cover;
+                                background-position: center;">
+                                        <div class="post-meta">
+
+                                            <div class="left-box">
+                                                <span>1 min Read</span>
+                                                <span class="date">Jan 01, 2023</span>
+                                            </div>
+
+                                            <div class="right-box">
+                                                <span>Shadow IT</span>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <h3 class="title">Healthcare Data Actively Targeted and Sold on the Dark Web</h3>
+                                    <p>How do you create compelling presentations that wow your colleagues and impress
+                                        your
+                                        managers?</p>
+                                    <a href="#" class="link">Read post</a>
+                                </article>
+                            </div>
+
+
+                            <div class="item">
+                                <article>
+                                    <div class="image-container" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/resource-img.jpg); background-size: cover;
+                                background-position: center;">
+                                        <div class="post-meta">
+
+                                            <div class="left-box">
+                                                <span>1 min Read</span>
+                                                <span class="date">Jan 01, 2023</span>
+                                            </div>
+
+                                            <div class="right-box">
+                                                <span>Shadow IT</span>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <h3 class="title">Healthcare Data Actively Targeted and Sold on the Dark Web</h3>
+                                    <p>How do you create compelling presentations that wow your colleagues and impress
+                                        your
+                                        managers?</p>
+                                    <a href="#" class="link">Read post</a>
+                                </article>
+                            </div>
+
+                            <div class="item">
+                                <article>
+                                    <div class="image-container" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/resource-img.jpg); background-size: cover;
+                                background-position: center;">
+                                        <div class="post-meta">
+
+                                            <div class="left-box">
+                                                <span>1 min Read</span>
+                                                <span class="date">Jan 01, 2023</span>
+                                            </div>
+
+                                            <div class="right-box">
+                                                <span>Shadow IT</span>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <h3 class="title">Healthcare Data Actively Targeted and Sold on the Dark Web</h3>
+                                    <p>How do you create compelling presentations that wow your colleagues and impress
+                                        your
+                                        managers?</p>
+                                    <a href="#" class="link">Read post</a>
+                                </article>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="partners-logo" style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/images/bg-2.png);">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content-box">
+                    <h2 class="title">Built on trust since 2013</h2>
+                    <p>We take pride in protecting a global network of 100’s of enterprise clients.</p>
+                </div>
+                <div class="logo-container swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="item">
+                                <a href="#">
+                                    <div class="image-container">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cushman.png"
+                                            alt="logo">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="item">
+                                <a href="#">
+                                    <div class="image-container">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/elkem.png"
+                                            alt="logo">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <div class="item">
+                                <a href="#">
+                                    <div class="image-container">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/lous-vuitton.png"
+                                            alt="logo">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <div class="item">
+                                <a href="#">
+                                    <div class="image-container">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/airfrance-logo.png"
+                                            alt="logo">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide">
+                            <div class="item">
+                                <a href="#">
+                                    <div class="image-container">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/technicolor.png"
+                                            alt="logo">
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="content-with-img-bluebg">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content-box">
+                    <h2 class="title">Experience the CybelAngel Difference</h2>
+                    <div class="content">
+                        <p>Our Data Breach Prevention and Asset Discovery and Monitoring solutions can defuse your
+                            supply chain attack risk.</p>
+                    </div>
+
+
+                    <div class="btn-container">
+                        <ul>
+                            <li><a class="btn btn-white" href="#">Learn more</a></li>
+                            <li><a class="btn btn-sweet" href="#">Get Started</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="image-box">
+                    <div class="image-container">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Screenmockup.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<?php
+get_footer();
