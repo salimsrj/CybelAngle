87267498178975e2585e40a86bfb4bfa0a5ce6bd
Mahmudul Hasan <salimsrj@gmail.com>
2023-03-12 12:12:57 +0600

    Menu icon has been added by acf options

--- functions.php
+++ functions.php
@@ -220,3 +220,28 @@ function my_acf_block_render_callback( $block ) {
     }
 }
 
+
+add_filter('wp_nav_menu_objects', 'my_wp_nav_menu_objects', 10, 2);
+
+function my_wp_nav_menu_objects( $items, $args ) {
+    
+    // loop
+    foreach( $items as &$item ) {
+        
+        // vars
+        $icon = get_field('icon', $item);
+        
+        
+        // append icon
+        if( $icon ) {
+            
+            $item->title = '<span class="icon"><img src="'.$icon['url'].'"></span> '.$item->title;
+            
+        }
+        
+    }
+    
+        return $items;
+    
+}
+
