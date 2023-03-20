d6597fac68b20a782e4acf7863d1e217fd08ef4e
Mahmudul Hasan <salimsrj@gmail.com>
2023-03-21 04:48:21 +0600

    Block Carousel has been dynauthorauthor

--- global-templates/block/content-carousel.php
+++ global-templates/block/content-carousel.php
@@ -4,81 +4,61 @@
  *
  */
 
-$title = get_field('title');
+$header = get_field('header');
+$header_title = $header['title'];
+$header_content = $header['content'];
+$header_buttons = $header['buttons'];
+$button_one = $header_buttons['button_one'];
+$button_two = $header_buttons['button_two'];
+$carousel = get_field('carousel');
 ?>
 <section class="button-with-carousel">
     <div class="container">
+        <?php if($header): ?>
         <div class="row section-head">
             <div class="col-md-7">
                 <div class="">
-                    <h2 class="title">Case Studies</h2>
-                    <p>Dig into our clients’ successes in these case studies.</p>
+                    <?php if($header_title != ''): ?>
+                        <h2 class="title"><?php echo $header_title; ?></h2>
+                    <?php endif; ?>
+                    <?php if($header_content != ''): ?>
+                        <p><?php echo $header_content; ?></p>
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="col-md-5">
+                <?php if($header_buttons): ?>
                 <div class="button-container">
                     <ul>
-                        <li><a href="#" class="btn btn-white">Request Your Trial</a></li>
-                        <li><a href="#" class="btn btn-sweet">See More Successes Here</a></li>
+                        <?php if($button_one): ?>
+                        <li><a href="<?php echo $button_one['url']; ?>" class="btn btn-white"><?php echo $button_one['label']; ?></a></li>
+                        <?php endif; ?>
+                        <?php if($button_two): ?>
+                        <li><a href="<?php echo $button_two['label']; ?>" class="btn btn-sweet"><?php echo $button_two['label']; ?></a></li>
+                        <?php endif; ?>
                     </ul>
                 </div>
+                <?php endif; ?>
             </div>
         </div>
+        <?php endif; ?>
+
         <div class="row">
             <div class="col-12">
-                <div class="carousel-box swiper">
+                <div class="carousel-box swiper">                    
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="item"
-                                style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/images/airbus.jpg);">
-                                <div class="content-box">
-                                    <p>AirFrance Secures Data Leak Protecting Intellectual Property</p>
-                                    <span class="item-footer">Signify x CybelAngel</span>
+                        
+                        <?php foreach($carousel as $slide): ?>
+                            <div class="swiper-slide">
+                                <div class="item"
+                                    style="background:url(<?php echo $slide['image']['url']; ?>);">
+                                    <div class="content-box">
+                                        <p><?php echo $slide['title'] ?></p>
+                                        <span class="item-footer"><?php echo $slide['author'] ?></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="swiper-slide">
-                            <div class="item"
-                                style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/images/carousel_2.jpg);">
-                                <div class="content-box">
-                                    <p>AirFrance Secures Data Leak Protecting Intellectual Property</p>
-                                    <span class="item-footer">Signify x CybelAngel</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="swiper-slide">
-                            <div class="item"
-                                style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/images/carousel_3.jpg);">
-                                <div class="content-box">
-                                    <p>AirFrance Secures Data Leak Protecting Intellectual Property</p>
-                                    <span class="item-footer">Signify x CybelAngel</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="swiper-slide">
-                            <div class="item"
-                                style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/images/carousel_2.jpg);">
-                                <div class="content-box">
-                                    <p>AirFrance Secures Data Leak Protecting Intellectual Property</p>
-                                    <span class="item-footer">Signify x CybelAngel</span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="swiper-slide">
-                            <div class="item"
-                                style="background:url(<?php echo get_stylesheet_directory_uri(); ?>/images/carousel_3.jpg);">
-                                <div class="content-box">
-                                    <p>AirFrance Secures Data Leak Protecting Intellectual Property</p>
-                                    <span class="item-footer">Signify x CybelAngel</span>
-                                </div>
-                            </div>
-                        </div>
-
-
+                        <?php endforeach; ?>
 
                     </div>
                     <div class="navigation">
